5f8c9d151ccd0b52375bf71585ebd7d5fa47a2dd
lucho <lucianoinserra45@gmail.com>
2024-10-21 21:24:37 -0300

    prueba

---
+++ js/codigo.js
@@ -0,0 +1,19 @@
+alert('Hola mundo desde JS');
+
+let numero = -12;
+let numero2 = 2.635;
+
+let direccion = 'San Martin 1320';
+
+let estado = true;
+
+// Esto es un comentario
+// var
+let nombre = prompt('Ingrese su nombre: ')
+
+if (nombre == 'root') {
+    alert('Acceso correcto')
+}
+else {
+    alert('Acceso denegado')
+}
